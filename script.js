b20a6c5e20eb0d5a895aec9fa2902794d5e254bf
jsales5 <222031377@aluno.unb.br>
2026-03-03 12:56:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
-const dependencias = {
-    'MAT0025': ['MAT0026'],
+const fluxo = {
+    // 1 -> 2
+    'CEL0088': ['ECL0033'],
     'IFD0171': ['IFD0175'],
     'IFD0173': ['IFD0177'],
-    'MAT0026': ['MAT0027', 'ENC0035'], // Cálculo 2 libera Cálculo 3 e Mec. Sólidos
-    'IFD0175': ['ENC0035'],            // Física 2 também libera Mec. Sólidos
-    'IGD0173': ['ENC0241'],            // Geologia libera Geotecnia 1
-    'ENC0241': ['ENC0235'],            // Geotecnia 1 libera 2
-    'ENC0268': ['ENC0037'],            // Ciência dos Mat. libera Transf. Energia
-    'ENC0028': ['ENC0030'],            // Projeto Final 1 libera o 2
-    'ENC0026': ['ENC0031']             // Impactos libera Gestão Ambiental
+    'MAT0025': ['MAT0026'],
+    // 2 -> 3
+    'MAT0026': ['MAT0027', 'ENC0035'],
+    'IFD0175': ['ENC0035'],
+    'IGD0173': ['ENC0241', 'ENC0240'],
+    'IQD0125': ['ENC0268'],
+    // 3 -> 4
+    'MAT0027': ['ENC0274', 'ENC0275'],
+    'ENC0035': ['ENC0037'],
+    'ENC0241': ['ENC0235'],
+    'ENC0268': ['ENC0037'],
+    'ENC0269': ['ENC0270'],
+    // 4 -> 5
+    'ENC0235': ['ENC0183'],
+    'ENC0037': ['ENC0251', 'ENC0252'],
+    // 5 -> 6
+    'ENC0251': ['ENC0166', 'ENC0001'],
+    // 6 -> 7
+    'ENC0166': ['ENC0004', 'ENC0007', 'ENC0010', 'ENC0052'],
+    // 7 -> 8
+    'ENC0007': ['ENC0015'],
+    'ENC0052': ['ENC0016'],
+    // 9 -> 10
+    'ENC0028': ['ENC0030'],
+    'ENC0026': ['ENC0031']
 };
 
-document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
-    checkbox.addEventListener('change', function() {
-        const idMateria = this.parentElement.id;
-        const liberados = dependencias[idMateria];
+document.querySelectorAll('input[type="checkbox"]').forEach(check => {
+    check.addEventListener('change', function() {
+        const materiaId = this.parentElement.id;
+        const dependentes = fluxo[materiaId];
 
-        if (this.checked) {
-            this.parentElement.classList.add('concluida');
-        } else {
-            this.parentElement.classList.remove('concluida');
-        }
+        this.parentElement.classList.toggle('concluida', this.checked);
 
-        if (liberados) {
-            liberados.forEach(idAlvo => {
-                const alvo = document.querySelector(`#${idAlvo} input`);
-                if (alvo) {
-                    alvo.disabled = !this.checked;
+        if (dependentes) {
+            dependentes.forEach(id => {
+                const el = document.querySelector(`#${id} input`);
+                if (el) {
+                    el.disabled = !this.checked;
                     if (!this.checked) {
-                        alvo.checked = false;
-                        alvo.parentElement.classList.remove('concluida');
-                        alvo.dispatchEvent(new Event('change'));
+                        el.checked = false;
+                        el.parentElement.classList.remove('concluida');
+                        el.dispatchEvent(new Event('change'));
                     }
                 }
             });
