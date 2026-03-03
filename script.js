b4df3fa2ec10a269d9a44b2e7d23b5ac3471dae6
jsales5 <222031377@aluno.unb.br>
2026-03-03 13:23:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,73 @@
-const dependencias = {
-    // 1 -> 2
-    'CEL0088': ['ECL0033'],
-    'IFD0171': ['IFD0175'],
-    'IFD0173': ['IFD0177'],
-    'MAT0025': ['MAT0026'],
-    'IGD0173': ['ENC0241', 'ENC0240'],
-    'IQD0125': ['ENC0268', 'ENC0272', 'ENC0273'],
-    // 2 -> 3
-    'MAT0026': ['MAT0027', 'ENC0035'],
-    'IFD0175': ['ENC0035'],
-    'MAT0031': ['ENC0275'],
-    // 3 -> 4
-    'ENC0035': ['ENC0037'],
-    'ENC0268': ['ENC0037'],
-    'ENC0241': ['ENC0235'],
-    'MAT0027': ['ENC0274', 'ENC0275'],
-    'ENC0263': ['ENC0269', 'ENC0270'],
-    // 4 -> 5
-    'ENC0235': ['ENC0183'],
-    'ENC0037': ['ENC0251', 'ENC0252'],
-    // 5 -> 6
-    'ENC0251': ['ENC0166', 'ENC0001'],
-    'ENE0001': ['EPR0068'],
-    // 6 -> 7
-    'ENC0166': ['ENC0004', 'ENC0007', 'ENC0010', 'ENC0052'],
-    'EPR0068': ['EPR0059'],
-    // 7 -> 8
-    'ENC0004': ['ENC0013'],
-    'ENC0007': ['ENC0015'],
-    'ENC0052': ['ENC0016'],
-    // 8 -> 9
-    'ENC0013': ['ENC0026'],
-    'ENC0025': ['ENC0028'],
-    // 9 -> 10
-    'ENC0028': ['ENC0030'],
-    'ENC0026': ['ENC0031']
+const regras = {
+    // Matéria: [Lista de IDs que ela precisa para ser liberada]
+    'ECL0033': ['CEL0088'],
+    'IFD0175': ['IFD0171'],
+    'IFD0177': ['IFD0173'],
+    'MAT0026': ['MAT0025'],
+    'ENC0035': ['MAT0026', 'IFD0175'],
+    'MAT0027': ['MAT0026'],
+    'ENC0240': ['IGD0173'],
+    'ENC0241': ['IGD0173'],
+    'ENC0268': ['IQD0125'],
+    'ENC0037': ['ENC0035', 'ENC0268'],
+    'ENC0235': ['ENC0241'],
+    'ENC0269': ['ENC0263'],
+    'ENC0270': ['ENC0263'],
+    'ENC0272': ['IQD0125'],
+    'ENC0273': ['IQD0125'],
+    'ENC0183': ['ENC0235'],
+    'ENC0251': ['ENC0037'],
+    'ENC0252': ['ENC0037'],
+    'ENC0274': ['MAT0027'],
+    'ENC0275': ['MAT0027', 'MAT0031'],
+    'ENC0001': ['ENC0251'],
+    'ENC0002': ['SOL0042'],
+    'ENC0166': ['ENC0251'],
+    'ENE0001': ['IFD0175'],
+    'EPR0068': ['MAT0031'],
+    'ENC0004': ['ENC0166'],
+    'ENC0007': ['ENC0001', 'ENC0166'],
+    'ENC0010': ['ENC0166'],
+    'ENC0052': ['ENC0166'],
+    'EPR0059': ['EPR0068'],
+    'FDD0282': ['SOL0042'],
+    'ENC0015': ['ENC0007', 'ENC0272'],
+    'ENC0016': ['ENC0052'],
+    'ENC0055': ['ECL0033'],
+    'ENC0025': ['EPR0059'],
+    'ENC0026': ['ENC0004', 'FDD0282'],
+    'ENC0028': ['ENC0025'],
+    'ENC0030': ['ENC0028'],
+    'ENC0031': ['ENC0026', 'ENC0015', 'ENC0016']
 };
+
+function verificarBloqueios() {
+    Object.keys(regras).forEach(materiaId => {
+        const preRequisitos = regras[materiaId];
+        const inputMateria = document.querySelector(`#${materiaId} input`);
+        
+        // Verifica se todos os pré-requisitos estão marcados
+        const todosConcluidos = preRequisitos.every(id => {
+            const preCheck = document.querySelector(`#${id} input`);
+            return preCheck && preCheck.checked;
+        });
+
+        if (inputMateria) {
+            inputMateria.disabled = !todosConcluidos;
+            if (!todosConcluidos) {
+                inputMateria.checked = false;
+                inputMateria.parentElement.classList.remove('concluida');
+            }
+        }
+    });
+}
 
 document.querySelectorAll('input[type="checkbox"]').forEach(check => {
     check.addEventListener('change', function() {
-        const idMateria = this.parentElement.id;
-        const liberados = dependencias[idMateria];
-        
         this.parentElement.classList.toggle('concluida', this.checked);
-
-        if (liberados) {
-            liberados.forEach(idAlvo => {
-                const inputAlvo = document.querySelector(`#${idAlvo} input`);
-                if (inputAlvo) {
-                    inputAlvo.disabled = !this.checked;
-                    if (!this.checked) {
-                        inputAlvo.checked = false;
-                        inputAlvo.parentElement.classList.remove('concluida');
-                        inputAlvo.dispatchEvent(new Event('change'));
-                    }
-                }
-            });
-        }
+        verificarBloqueios(); // Reavalia o fluxo inteiro a cada clique
     });
 });
+
+// Inicializa o estado dos campos ao carregar a página
+verificarBloqueios();
